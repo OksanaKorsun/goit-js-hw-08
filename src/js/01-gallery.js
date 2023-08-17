83c57e4fae21452f9d18a708b2d07161217deae1
OksanaKorsun <oksana2081989@gmail.com>
2023-08-17 23:22:22 +0300

    3

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,25 @@
+// Описаний в документації
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+const imgList = galleryItems.map(({ preview, original, description }) => `
+        <li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" 
+      src="${preview}" 
+      alt="${description}" />
+   </a>
+</li>`).join(' ');
+
+gallery.insertAdjacentHTML('afterbegin', imgList);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250
+})
