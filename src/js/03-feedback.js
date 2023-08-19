996de0222901e99e24bcd1cef37bcf1a82eb9b6f
OksanaKorsun <oksana2081989@gmail.com>
2023-08-19 20:59:15 +0300

    6

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,19 +1,25 @@
 import throttle from 'lodash.throttle';
 
 const form = document.querySelector('.feedback-form');
+const LOCAL_KEY = 'feedback-form-state';
 
-const emailInput = form.elements.email;
-const messageInput = form.elements.message;
-const feedback = {
-  email: emailInput.value,
-  message: messageInput.value,
-};
-
-console.log(feedback);
-console.log(form);
+const userData = JSON.parse(localStorage.getItem(LOCAL_KEY)) ?? {};
 
 form.addEventListener('input', throttle(handlerInput, 500));
-const LOCAL_KEY = 'feedback-form-state';
-function handlerInput() {
-  localStorage.setItem(LOCAL_KEY, feedback);
+form.addEventListener('submit', handlerSubmit);
+// document.addEventListener('DOMContentLoaded', saveInput)
+
+form.elements.email.value = userData.email || '';
+form.elements.message.value = userData.message || '';
+
+function handlerInput(event) {
+  userData[event.target.name] = event.target.value;
+  localStorage.setItem(LOCAL_KEY, JSON.stringify(userData));
+}
+
+function handlerSubmit(event) {
+  event.preventDefault();
+  console.log(userData);
+  event.currentTarget.reset();
+  localStorage.removeItem(LOCAL_KEY);
 }
